3548bfab84978ddca4f0787007f6bbfffd9390a6
SBHattarj <subhambhsubhambh@gmail.com>
2022-05-08 13:59:57 +0000

    fixed start

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-make poetry-download
+cd /home/gitpod/.pyenv/plugins/python-build/../.. && git pull && cd -
 pyenv install 3.9.0
 pyenv local 3.9.0
+make poetry-download
 make install && make codestyle
